eb3052e72e3a8577cdbfa53d8e5fac488c2422f0
Nilaksh Patil <nilakshpatil@gmail.com>
2021-06-24 11:14:10 -0400

    I added one box

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,box7;
 var backgroundImg,platform;
 
 function preload() {
@@ -33,6 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    box7 = new Box(830,200,60,60);
 
     bird = new Bird(100,100);
 
@@ -58,6 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    box7.display();
 
     bird.display();
     platform.display();
